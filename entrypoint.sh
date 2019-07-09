a2d71cb8bcd3b0d68d9008ca0a39ba4d98530e7f
mkorejo <mkorejo@gmail.com>
2019-07-08 23:47:40 -0500

    updates for multiprocess file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sh /wait-for-it.sh $POSTGRES_HOSTNAME:$POSTGRES_PORT
 
-PROPS_FILE='/usr/share/manifoldcf/example/properties.xml'
+PROPS_FILE='./properties.xml'
 
 sed -i "s/\$MANIFOLDCF_VERSION/$MANIFOLDCF_VERSION/" $PROPS_FILE
 sed -i "s/\$POSTGRES_HOSTNAME/$POSTGRES_HOSTNAME/" $PROPS_FILE
@@ -12,11 +12,19 @@ sed -i "s/\$POSTGRES_USER/$POSTGRES_USER/" $PROPS_FILE
 sed -i "s/\$POSTGRES_PASSWORD/$POSTGRES_PASSWORD/" $PROPS_FILE
 
 if [ "$1" = 'start' ]; then
-  exec java -jar start.jar
+  chmod +x *.sh
 
+  echo "Initializing DB ..."
+  sh initialize.sh
+
+  echo "Starting agents ..."
+  sh start-agents.sh &
+
+  echo "Starting web application ..."
+  sh start-webapps.sh
 elif [ "$1" = 'sleep' ]; then
   while true; do
     echo "running sleep";
     sleep 10;
   done;
-fi;
+fi
